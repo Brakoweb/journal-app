92d813476091fa34cd2b5618025b5de107762b53
Gabriel Bracamonte <gabobrako@gmail.com>
2021-12-23 15:56:56 -0500

    initializing app and creating routers

---
+++ src/routers/AppRouter.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter,Routes, Route, Navigate  } from 'react-router-dom';
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+import { JournalScreen } from '../components/journal/JournalScreen';
+
+export const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/auth'> 
+                    <Route path='login' element={<LoginScreen />} />
+                    <Route path='register' element={<RegisterScreen />} />
+                </Route>
+
+                <Route index path='/' element={<JournalScreen />} />
+
+                <Route path='*' element={<Navigate to='/' />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
