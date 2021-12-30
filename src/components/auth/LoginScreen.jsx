6850cc6e927eea3b1da11898eddd37f42b4e5b4a
Gabriel Bracamonte <gabobrako@gmail.com>
2021-12-30 04:32:04 -0500

    adding firebase auth

--- src/components/auth/LoginScreen.jsx
+++ src/components/auth/LoginScreen.jsx
@@ -1,15 +1,47 @@
 import React from 'react'
 import {Link} from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+import { useForm } from '../../hooks/useForm';
+import { login } from '../../actions/auth';
 
 export const LoginScreen = () => {
+
+    const dispach = useDispatch();
+
+    const [formValues, handleInputChange] = useForm({
+        email: 'brako@gmail.com',
+        password: '123'
+    });
+
+    const {email, password} = formValues;
+
+    const handleLogin = (e) =>{
+        e.preventDefault();
+        dispach(login(12345,'Gabriel'));
+
+    }
     return (
         <div className='auth__main'>
             <div className='auth__box-container'>
                 <h3 className='auth__title mb-5'>Login</h3>
 
-                <form>
-                    <input type="text" name="email" placeholder='Email' className='auth__input' />
-                    <input type="password" name="password" placeholder='Password' className='auth__input' />
+                <form onSubmit={handleLogin}>
+                    <input 
+                        type="text" 
+                        name="email" 
+                        placeholder='Email' 
+                        className='auth__input'
+                        value={email}
+                        onChange={handleInputChange}
+                    />
+                    <input 
+                        type="password" 
+                        name="password" 
+                        placeholder='Password' 
+                        className='auth__input' 
+                        value={password}
+                        onChange={handleInputChange}
+                    />
                     <button
                         type='submit'
                         className='btn btn-primary btn-block'
